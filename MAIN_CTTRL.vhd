ab92ebaac8753eddc8fb445b2f465e2f09fe46a4
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2015-12-09 20:40:17 -0200

    adding pipeline signals

--- MAIN_CTTRL.vhd
+++ MAIN_CTTRL.vhd
@@ -210,6 +210,92 @@ ARCHITECTURE ARC_MAIN_PROCESSOR OF MAIN_PROCESSOR IS
 			ZERO : 				OUT  	STD_LOGIC	
 		);
 	END COMPONENT;
+
+	COMPONENT IF_ID IS
+		PORT (instruction : in std_logic_vector(31 downto 0);
+          	pcplus4     : in std_logic_vector(31 downto 0);
+          	clk         : in std_logic;
+          	reset       : in std_logic;
+          	instr_out   : out std_logic_vector(31 downto 0);
+          	pc_out      : out std_logic_vector(31 downto 0));
+    END COMPONENT;
+
+    COMPONENT ID_EX IS
+		PORT (clk           : in std_logic;
+	          RegWrite   : in std_logic;
+	          MemtoReg   : in std_logic;
+	          MemWrite   : in std_logic;
+	          Branch     : in std_logic_vector(31 downto 0);
+	          ALUOp : in std_logic_vector(2 downto 0);
+	          ALUSrc     : in std_logic;
+	          RegDst     : in std_logic;
+
+	          RD1           : in std_logic_vector(31 downto 0);
+	          RD2           : in std_logic_vector(31 downto 0);
+	          RtE           : in std_logic_vector(4 downto 0);
+	          RdE           : in std_logic_vector(4 downto 0);
+
+	          SignExt       : in std_logic_vector(31 downto 0);
+	          PCPlus4       : in std_logic_vector(31 downto 0);
+
+	          outRegWrite   : out std_logic;
+	          outMemtoReg   : out std_logic;
+	          outMemWrite   : out std_logic;
+	          outBranch     : out std_logic_vector(31 downto 0);
+	          outALUOp : out std_logic_vector(2 downto 0);
+	          outALUSrc     : out std_logic;
+	          outRegDst     : out std_logic;
+
+	          outRD1           : out std_logic_vector(31 downto 0);
+	          outRD2           : out std_logic_vector(31 downto 0);
+	          outRtE           : out std_logic_vector(4 downto 0);
+	          outRdE           : out std_logic_vector(4 downto 0);
+
+	          outSignExt       : out std_logic_vector(31 downto 0);
+	          outPCPlus4       : out std_logic_vector(31 downto 0));
+    END COMPONENT;
+
+    COMPONENT EX_MEM IS
+		PORT (clk           : in std_logic;
+	          RegWrite   : in std_logic;
+	          MemtoReg   : in std_logic;
+	          MemWrite   : in std_logic;
+	          Branch     : in std_logic_vector(31 downto 0);
+	          
+	          ZeroM         : in std_logic;
+	          
+	          AluOutM       : in std_logic_vector(31 downto 0); --SAIDA DA ULA
+	          WriteDataM    : in std_logic_vector(31 downto 0); -- VEM DA SAIDA 2 DE REG
+	          WriteRegM     : in std_logic_vector(4 downto 0); -- REG DESTINO VEM DO MX_1
+	          PcBranchM     : in std_logic_vector(31 downto 0); --ENDERECO DE DESVIO CONDICIONAL
+	          
+	          outRegWrite   : out std_logic;
+	          outMemtoReg   : out std_logic;
+	          outMemWrite   : out std_logic;
+	          outBranch     : out std_logic_vector(31 downto 0);
+	          outZeroM         : out std_logic;
+	          outAluOutM       : out std_logic_vector(31 downto 0);
+	          outWriteDataM    : out std_logic_vector(31 downto 0);
+	          outWriteRegM     : out std_logic_vector(4 downto 0);
+	          outPcBranchM     : out std_logic_vector(31 downto 0));
+    END COMPONENT;
+
+    COMPONENT MEM_WB IS
+		PORT (clk           : in std_logic;
+	          RegWrite   : in std_logic;
+	          MemtoReg   : in std_logic;
+
+	          ReadDataW     : in std_logic_vector(31 downto 0);
+	          AluOutW       : in std_logic_vector(31 downto 0);
+	          WriteRegW     : in std_logic_vector(4 downto 0);
+
+	          outRegWrite : out std_logic;
+	          outMemtoReg : out std_logic;
+
+	          outReadDataW   : out std_logic_vector(31 downto 0);
+	          outAluOutW     : out std_logic_vector(31 downto 0);
+	          outWriteRegW   : out std_logic_vector(4 downto 0));
+    END COMPONENT;
 	
 	--ADD_PC
 	SIGNAL S_ADD_PC_OUT_A : 		STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -278,8 +364,52 @@ ARCHITECTURE ARC_MAIN_PROCESSOR OF MAIN_PROCESSOR IS
 	SIGNAL S_ULA_CTRL_OUT_A : 		STD_LOGIC_VECTOR (2 DOWNTO 0);
 	
 	--ULA
-	SIGNAL S_ULA_OUT_A :				STD_LOGIC_VECTOR (31 downto 0);
+	SIGNAL S_ULA_OUT_A :				STD_LOGIC_VECTOR (31 DOWNTO 0);
 	SIGNAL S_ULA_ZERO :  			STD_LOGIC;
+
+
+
+	------------------------  PIPE  ----------------------------
+	--IF_ID
+	SIGNAL  S_PCPlus4_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_INSTRUCTION_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+
+	--ID_EX
+	SIGNAL  S_RegWrite_IDEX_OUT :				STD_LOGIC;
+	SIGNAL  S_MemtoReg_IDEX_OUT :				STD_LOGIC;
+	SIGNAL  S_MemWrite_IDEX_OUT :				STD_LOGIC;
+	SIGNAL  S_Branch_IDEX_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_ALUOp_OUT :				STD_LOGIC_VECTOR (2 DOWNTO 0);
+	SIGNAL  S_ALUSrc_OUT :				STD_LOGIC;
+	SIGNAL  S_RegDst_OUT :				STD_LOGIC;
+	SIGNAL  S_RD1_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_RD2_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_RtE_OUT :				STD_LOGIC_VECTOR (4 DOWNTO 0);
+	SIGNAL  S_RdE_OUT :				STD_LOGIC_VECTOR (4 DOWNTO 0);
+	SIGNAL  S_SignExt_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_PCPlus4_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+
+	--EX_MEM
+	SIGNAL  S_RegWrite_EXMEM_OUT :				STD_LOGIC;
+	SIGNAL  S_MemtoReg_EXMEM_OUT :				STD_LOGIC;
+	SIGNAL  S_MemWrite_OUT :				STD_LOGIC;
+	SIGNAL  S_Branch_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_ZERO_OUT :				STD_LOGIC;
+	SIGNAL  S_AluOutM_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_WriteDataM_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_WriteRegM_OUT :				STD_LOGIC_VECTOR (4 DOWNTO 0);
+	SIGNAL  S_PcBranchM_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+
+
+	--MEM_WB
+	SIGNAL  S_RegWrite_MEMWB_OUT :				STD_LOGIC;
+	SIGNAL  S_MemtoReg_MEMWB_OUT :				STD_LOGIC;
+	SIGNAL  S_ReadDataW_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_RAluOutW_OUT :				STD_LOGIC_VECTOR (31 DOWNTO 0);
+	SIGNAL  S_WriteRegW_OUT :				STD_LOGIC_VECTOR (4 DOWNTO 0);
+
+
+
 	
 	--DEMAIS SINAIS
 	SIGNAL S_GERAL_OPCode :			STD_LOGIC_VECTOR(5 DOWNTO 0);
@@ -321,6 +451,9 @@ BEGIN
 	C_MEM :					MEM PORT MAP(CLK, RESET, S_CTRL_MemWrite, S_CTRL_MemRead, S_ULA_OUT_A, S_REG_OUT_B, S_MEM_OUT_A);
 	C_MX_4 :					MX_4 PORT MAP(S_CTRL_Jump, S_CONCAT_OUT_A, S_MX_3_OUT_A, S_MX_4_OUT_A);
 	C_MX_5 :					MX_5 PORT MAP(S_CTRL_MemtoReg, S_MEM_OUT_A, S_ULA_OUT_A, S_SL_16_OUT_A, S_MX_5_OUT_A);
+
+	--PIPE
+	C_IF_ID :					IF_ID PORT MAP(CLK, S_ADD_PC_OUT_A, S_INST_OUT_A, S_PCPlus4_OUT, S_INSTRUCTION_OUT);
 	
 END ARC_MAIN_PROCESSOR;
 
